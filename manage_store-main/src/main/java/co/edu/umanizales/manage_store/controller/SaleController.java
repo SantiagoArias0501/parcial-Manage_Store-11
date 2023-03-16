e6b2e561c085671372e61842b9544ede5ea32df1
SantiagoArias0501 <101849879+SantiagoArias0501@users.noreply.github.com>
2023-03-15 22:29:10 -0500

    Add files via upload

---
+++ manage_store-main/src/main/java/co/edu/umanizales/manage_store/controller/SaleController.java
@@ -0,0 +1,138 @@
+package co.edu.umanizales.manage_store.controller;
+
+import co.edu.umanizales.manage_store.controller.dto.BestSellerDTO;
+import co.edu.umanizales.manage_store.controller.dto.BestStoreDTO;
+import co.edu.umanizales.manage_store.controller.dto.ResponseDTO;
+import co.edu.umanizales.manage_store.controller.dto.SaleDTO;
+import co.edu.umanizales.manage_store.model.Sale;
+import co.edu.umanizales.manage_store.model.Seller;
+import co.edu.umanizales.manage_store.model.Store;
+import co.edu.umanizales.manage_store.service.SaleService;
+import co.edu.umanizales.manage_store.service.SellerService;
+import co.edu.umanizales.manage_store.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "sale")
+public class SaleController {
+    @Autowired
+    private SaleService saleService;
+    @Autowired
+    private SellerService sellerService;
+    @Autowired
+    private StoreService storeService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO> getSales() {
+        return new ResponseEntity<>(
+                new ResponseDTO(200,
+                        saleService.getSales(),
+                        null),
+                HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/total")
+    public ResponseEntity<ResponseDTO> getTotalSales() {
+        if (saleService.getTotalSales() != 0) {
+            return new ResponseEntity<>(new ResponseDTO(200, saleService.getTotalSales(), null), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new ResponseDTO(409, "no hay ventas", null), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @GetMapping(path = "/totalsalesbyseller/{code}")
+    public ResponseEntity<ResponseDTO> getTotalSalesBySeller(
+            @PathVariable String code
+    ) {
+        return new ResponseEntity<>(new ResponseDTO(200, saleService.getTotalSales(), null), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/totalStore/{code}")
+    public ResponseEntity<ResponseDTO> getTotalSalesByStore(
+            @PathVariable String code
+    ) {
+        return new ResponseEntity<>(new ResponseDTO(200, saleService.getTotalSalesByStore(code), null), HttpStatus.OK);
+    }
+
+    @PostMapping
+
+    public ResponseEntity<ResponseDTO> createSale(@RequestBody
+                                                  SaleDTO saleDTO) {
+        Seller findSeller = sellerService.getSellerById(saleDTO.getSellerId());
+        if (findSeller == null) {
+            return new ResponseEntity<>(new ResponseDTO(409,
+                    "El vendedor ingresado no existe", null),
+                    HttpStatus.BAD_REQUEST);
+        }
+        Store findStore = storeService.getStoreById(saleDTO.getStoreId());
+        if (findStore == null) {
+            return new ResponseEntity<>(new ResponseDTO(409,
+                    "La tienda ingresada no existe", null),
+                    HttpStatus.BAD_REQUEST);
+        }
+        saleService.addSale(new Sale(findStore, findSeller,
+                saleDTO.getQuantity()));
+        return new ResponseEntity<>(new ResponseDTO(200,
+                "Venta adicionada", null),
+                HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/bestseller")
+    public ResponseEntity<ResponseDTO> getBestSeller() {
+        return new ResponseEntity<>(new ResponseDTO(200, (saleService.getBestSeller(sellerService.getSellers())),
+                null), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/beststore")
+    public ResponseEntity<ResponseDTO> getBestStore() {
+        return new ResponseEntity<>(new ResponseDTO(200, saleService.getBestStore(storeService.getStores()), null),
+                HttpStatus.OK);
+    }
+
+    //promedios de ventas por vendedores
+    @GetMapping(path = "/averagesalesbystore")
+    public ResponseEntity<ResponseDTO> getAverageSaleByStore() {
+        int findSale = saleService.getTotalSales();
+        while (findSale != 0) {
+            return new ResponseEntity<>(new ResponseDTO(200, saleService.getTotalSales() / (float) storeService.getStores().size(), null),
+                    HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(new ResponseDTO(409, "no hay ventas , no se puede obtener un promedio ", null),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping(path = "/averagesalesbyseller")
+    public ResponseEntity<ResponseDTO> getAverageSaleBySellers() {
+        int findSale = saleService.getTotalSales();
+        while (findSale != 0) {
+            return new ResponseEntity<>(new ResponseDTO(200, saleService.getTotalSales() / (float) sellerService.getSellers().size(), null),
+                    HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(new ResponseDTO(409, "no hay ventas , no se puede obtener un promedio ", null),
+                HttpStatus.BAD_REQUEST);
+
+    }
+//ejercicio 11 (0-5)
+    @GetMapping(path = "/cantidadmayorSeller")
+    public ResponseEntity<ResponseDTO>getcantidadmayorSeller(@PathVariable int quantity){
+        List<Store>StoreL = new ArrayList<>();
+        for (Sale sales : saleService.getSales()){
+            if (saleService.getTotalSalesByStore(sales.getStore().getCode())>quantity){
+
+                StoreL.add(sales.getStore());
+            }
+        }
+        return new
+                ResponseEntity<>(new ResponseDTO(200,StoreL,null),HttpStatus.OK);
+
+    }
+}
